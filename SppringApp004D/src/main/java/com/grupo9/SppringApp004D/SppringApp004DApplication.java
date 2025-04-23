d61e5be8626a5c58f9b20b7bd75e0becb219add1
FabianOsses21 <169071886+FabianOsses21@users.noreply.github.com>
2025-04-23 10:15:22 -0400

    Add files via upload

---
+++ SppringApp004D/src/main/java/com/grupo9/SppringApp004D/SppringApp004DApplication.java
@@ -0,0 +1,13 @@
+package com.grupo9.SppringApp004D;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SppringApp004DApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SppringApp004DApplication.class, args);
+	}
+
+}
---
+++ SppringApp004D/src/test/java/com/grupo9/SppringApp004D/SppringApp004DApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.SppringApp004D;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SppringApp004DApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
