5f292860e67327b0ee9f150219243e778ebd8379
cetecom_avaras <cetecom_avaras@duoc.cl>
2025-06-11 10:37:23 -0400

    Documentación Usuario

--- SppringApp004D/src/main/java/com/grupo9/SppringApp004D/Controller/UsuarioController.java
+++ SppringApp004D/src/main/java/com/grupo9/SppringApp004D/Controller/UsuarioController.java
@@ -3,42 +3,107 @@ package com.grupo9.SppringApp004D.Controller;
 
 import com.grupo9.SppringApp004D.Model.Usuario;
 import com.grupo9.SppringApp004D.Service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
+@Tag(name="Controlador Usuarios",description = "Servicio Rest Para la gestion de Usuarios")
 public class UsuarioController {
 
     @Autowired
     UsuarioService usuarioService;
 
     @GetMapping
-    public String getAllUsuarios(){
-
-        return usuarioService.getAllUsuarios();
+    @Operation(summary = "Obtener Usuarios",description = "Obtiene la lista completa de usuarios registrados en sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Retorna lista completa de usuarios"),
+            @ApiResponse(responseCode = "404",description = "No se encuentran datos")
+    })
+    public ResponseEntity<List<Usuario>> getAllUsuarios(){
+        List<Usuario> lista = usuarioService.getAllUsuarios();
+        if(lista.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity<>(lista,HttpStatus.OK);
+        }
     }
 
     @GetMapping("/{id}")
-    public String getUserById(@PathVariable int id){
-
-        return usuarioService.getUsuario(id);
+    @Operation(summary = "Obtener Usuario por ID",description = "Segun un ID entregado, obtiene al usuario registrado con ese ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Retorna Usuario"),
+            @ApiResponse(responseCode = "404",description = "No se encuentran datos")
+    })
+    @Parameter(description = "El ID del usuario", example = "123")
+    public ResponseEntity<Usuario> getUsuario(@PathVariable int id){
+        Usuario us = usuarioService.getUsuario(id);
+        if(us!=null){
+            return new ResponseEntity<>(us,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping
-    public String addUser(@RequestBody Usuario usuario){
-
-        return usuarioService.addUsuario(usuario);
+    @Operation(summary = "Agregar Usuario",description = "Permite registrar un usuario en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Usuario creado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Usuario.class))),
+            @ApiResponse(responseCode = "204",description = "No hay contenido en la solicitud")
+    })
+    public ResponseEntity<Usuario> addUser(@RequestBody Usuario user){
+        usuarioService.addUsuario(user);
+        if(usuarioService.getUsuario(user.getId())!=null){
+            return new ResponseEntity<>(user,HttpStatus.CREATED);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public String deleteUserById(@PathVariable int id){
-
-        return usuarioService.removeUsuario(id);
+    @Operation(summary="Eliminar Usuario por ID",description = "Elimina un usuario segun el ID registrado en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Retorna Usuario"),
+            @ApiResponse(responseCode = "404",description = "No se encuentran datos")
+    })
+    @Parameter(description = "El ID del usuario", example = "123")
+    public ResponseEntity<Void> deleteUser(@PathVariable int id){
+        if(usuarioService.getUsuario(id)!=null){
+            usuarioService.removeUsuario(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/{id}")
-    public String updateUser(@PathVariable int id, @RequestBody Usuario usuario){
-        return usuarioService.updateUsuario(id, usuario);
+    @Operation(summary = "Actualizar Usuario",description = "Permite actualizar los datos de un usuario segun su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Usuario creado",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Usuario.class))),
+            @ApiResponse(responseCode = "204",description = "No hay contenido en la solicitud")
+    })
+    @Parameter(description = "El ID del usuario", example = "123")
+    public ResponseEntity<Usuario> updateUser(@PathVariable int id,@RequestBody Usuario usuario){
+        if(usuarioService.getUsuario(id)!=null){
+            usuarioService.updateUsuario(id,usuario);
+            return  new ResponseEntity<>(usuario,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- SppringApp004D/src/main/java/com/grupo9/SppringApp004D/Service/UsuarioService.java
+++ SppringApp004D/src/main/java/com/grupo9/SppringApp004D/Service/UsuarioService.java
@@ -6,68 +6,38 @@ import com.grupo9.SppringApp004D.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UsuarioService {
 
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    public String getAllUsuarios() {
-        String output = "";
-        for (Usuario temp: usuarioRepository.findAll()) {
-            output += "Id del usuario: " + temp.getId() + "\n";
-            output += "Nombre del usuario: " + temp.getNombre() + "\n";
-            output += "password del usuario: " + temp.getPassword() + "\n";
-            output += "email del usuario: " + temp.getEmail() + "\n";
-            output += "direccion del usuario: " + temp.getDireccion() + "\n";
-        }
-        if(output.isEmpty()){
-            return "No se encontraron usuarios";
-        }
-        else {
-            return output;
-        }
+    public List<Usuario> getAllUsuarios() {
+        return usuarioRepository.findAll();
     }
 
-    public String getUsuario(int id) {
-        String output = "";
-            if (usuarioRepository.existsById(id)) {
-                Usuario buscado = usuarioRepository.findById(id).get();
-                output = "Id del usuario: " +buscado.getId() +"\n";
-                output += "Nombre del usuario: " + buscado.getNombre() + "\n";
-                output += "password del usuario: " + buscado.getPassword() + "\n";
-                output += "email del usuario: " + buscado.getEmail() + "\n";
-                output += "direccion del usuario: " + buscado.getDireccion() + "\n";
-                return output;
-            }
-        return "No existe el usuario";
+    public Usuario getUsuario(int id) {
+        return usuarioRepository.findById(id).get();
     }
 
     //Agregar usuario
-    public String addUsuario(Usuario usuario) {
-        usuarioRepository.save(usuario);
-        return "Usuario agregado con exito!";
+    public Usuario addUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
     }
 
-    public String removeUsuario(int id) {
-            if (usuarioRepository.existsById(id)) {
-                Usuario buscado= usuarioRepository.findById(id).get();
-                usuarioRepository.delete(buscado);
-                return "Usuario eliminado con exito!";
-            }
-        return "No existe el usuario";
+    public void removeUsuario(int id) {
+        usuarioRepository.deleteById(id);
     }
 
-    public String updateUsuario(int id, Usuario usuario) {
-        if (usuarioRepository.existsById(id)) {
-            Usuario buscado = usuarioRepository.findById(id).get();
-            buscado.setNombre(usuario.getNombre());
-            buscado.setPassword(usuario.getPassword());
-            buscado.setEmail(usuario.getEmail());
-            buscado.setDireccion(usuario.getDireccion());
-            usuarioRepository.save(buscado);
-            return "Usuario actualizado con exito!";
-        }
-        return "No existe el usuario";
+    public Usuario updateUsuario(int id, Usuario usuario) {
+        Usuario us =  usuarioRepository.findById(id).get();
+        us.setNombre(usuario.getNombre());
+        us.setPassword(usuario.getPassword());
+        us.setEmail(usuario.getEmail());
+        us.setDireccion(usuario.getDireccion());
+        usuarioRepository.save(us);
+        return us;
     }
 }
